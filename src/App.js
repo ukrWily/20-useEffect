d0e7935e94f9a96aad8dcbade4b29aa0bfa52784
”ukrWily” <ukrwily@gmail.com>
2024-08-12 20:54:34 +0300

    PC first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [todo, setTodo] = useState(null);
+  //
+  useEffect(() => {
+    console.log("Callback in useEffect called");
+
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => setTodo(json));
+  }, []);
+  console.log("App rendered");
+  console.log(todo);
+
+  return <div className="App"></div>;
 }
 
 export default App;
